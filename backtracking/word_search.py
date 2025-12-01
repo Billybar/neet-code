1d9cbbe1c4db91d3e4a203fc76fcab88d04a28f6
billybar <barchaim10@gmail.com>
2025-12-01 08:25:13 +0200

    finish backtrack

--- backtracking/word_search.py
+++ backtracking/word_search.py
@@ -3,40 +3,42 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # get firs word position
         row_len, col_len = len(board), len(board[0])
-        def dfs(letters, i,j):
-            if i >= len(board) or j >= len(board[0]):
-                return None
+        visited = set()
 
-            if word.startswith(tuple(letters)):
-                return None
-
-            if letters == word:
+        def dfs(pos, i, j):
+            # 1. Success Case
+            if pos == len(word):
                 return True
 
-            if i+1 < row_len:
-                letters.append(board[i+1][j])
-                dfs(letters, i+1,j)     # down
-            if i-1 >= 0:
-                letters.pop()
-                letters.append(board[i-1][j])
-                dfs(letters, i-1, j)    # up
+            # 2. Boundary Checks (Added i < 0 and j < 0)
+            if (i < 0 or i >= row_len or
+                    j < 0 or j >= col_len or
+                    pos >= len(word)):
+                return False
 
-            if j-1 >= 0:
-                letters.pop()
-                letters.append(board[i][j-1])
-                dfs(letters, i, j-1)    # left
+            # 3. Visited or Mismatch Check
+            if (i, j) in visited or board[i][j] != word[pos]:
+                return False
 
-            if j+1 <= col_len:
-                letters.pop()
-                letters.append(board[i][j+1])
-                dfs(letters, i, j+1)
+            # 4. Mark Visited
+            visited.add((i, j))
+
+            # 5. Recursive Step
+            res = (dfs(pos + 1, i + 1, j) or  # down
+                   dfs(pos + 1, i - 1, j) or  # up
+                   dfs(pos + 1, i, j - 1) or  # left
+                   dfs(pos + 1, i, j + 1))  # right
+
+            # 6. CRITICAL: Backtrack (Unmark visited)
+            visited.remove((i, j))
+
+            return res
 
         for i in range(row_len):
             for j in range(col_len):
-                if board[i][j] == word[0]:
-                    if dfs([board[i][j]],i,j):
+                if board[i][j] == word[0]:  # Optimization: only start if first letter matches
+                    if dfs(0, i, j):
                         return True
         return False
 
