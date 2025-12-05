cf68f1007e5d554b12679da25df85ebb9f1367e2
Billybar <barchaim10@gmail.com>
2025-12-05 05:28:38 +0200

    more graph practice

--- graphs/Max_Area_of_Island.py
+++ graphs/Max_Area_of_Island.py
@@ -2,21 +2,22 @@ from typing import List, Set
 
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
         rows, cols = len(grid), len(grid[0])
-        visited: Set[tuple[int, int]] = set()
-        islands_count = 0  # To count how many islands we find
+        counter: List[int] = [0,0]
 
         def dfs(row, col):
             # 1. Bounds check
             if row < 0 or row >= rows or col < 0 or col >= cols:
                 return
             # 2. Check if water OR already visited
-            if grid[row][col] == '0':
+            if grid[row][col] == 0:
                 return
 
             # 3. Mark as visited (Modify the grid directly)
-            grid[row][col] = '0'
+            grid[row][col] = 0
+            counter[0] += 1
 
             # 4. Visit neighbors
             dfs(row + 1, col)
@@ -27,20 +28,20 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 # If we find a piece of land '1', it's a NEW island
-                if grid[r][c] == '1':
-                    dfs(r, c)  # Sink the whole island
-                    islands_count += 1  # Increment counter
+                if grid[r][c] == 1:
+                    dfs(r, c)
+                    counter[1] = max(counter[1], counter[0])
+                    counter[0] = 0
 
-        return islands_count
+        return counter[1]
 
-
-grid: List[List[str]] = [
-    ["0","1","1","1","0"],
-    ["0","1","0","1","0"],
-    ["1","1","0","0","0"],
-    ["0","0","0","0","0"]
-  ]
+grid = [
+  [0,1,1,0,1],
+  [1,0,1,0,1],
+  [0,1,1,0,1],
+  [0,1,0,0,1]
+]
 
 sol = Solution()
-sol.numIslands(grid)
+sol.maxAreaOfIsland(grid)
 
